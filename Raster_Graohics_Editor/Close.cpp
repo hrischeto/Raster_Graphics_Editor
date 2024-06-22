80d39a62ba6e646953046efcc76205a23ae7b653
Hristina Krumova <152891241+hrischeto@users.noreply.github.com>
2024-06-22 23:56:45 +0300

    close command

--- Raster_Graohics_Editor/Close.cpp
+++ Raster_Graohics_Editor/Close.cpp
@@ -1,1 +1,8 @@
 #include "Close.h"
+
+Close::Close(unsigned session) :_session(session) {}
+
+void Close::execute(Vector<Session>& openedSessions)
+{
+	openedSessions.erase(_session-1);
+}
--- Raster_Graohics_Editor/Close.h
+++ Raster_Graohics_Editor/Close.h
@@ -2,5 +2,11 @@
 #include "Command.h"
 class Close : public Command
 {
+public:
+	Close(unsigned session);
+	void execute(Vector<Session>& openedSessions) override;
+
+private:
+	unsigned _session = 0;
 };
 
